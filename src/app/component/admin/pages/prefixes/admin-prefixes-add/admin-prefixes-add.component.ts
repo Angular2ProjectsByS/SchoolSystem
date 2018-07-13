85e10b1816315247c2ea1d9ef8f01d83d667bd0d
scorpion43 <scorpion7812@gmail.com>
2018-07-13 11:39:32 +0200

    auto-height sizing for prefixes-panel

--- src/app/component/admin/pages/prefixes/admin-prefixes-add/admin-prefixes-add.component.ts
+++ src/app/component/admin/pages/prefixes/admin-prefixes-add/admin-prefixes-add.component.ts
@@ -8,17 +8,62 @@ import { Component, OnInit } from '@angular/core';
 export class AdminPrefixesAddComponent implements OnInit {
 
   constructor() { }
-  private prefixesToSend : Array<string> = ['a', 'b'];
+  private prefixesToSend : Array<string> = ['a', 'b', 'b', 'dfasfadsfadsf', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b',
+  'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'];
   actualPrefix : string;
+  validationMessage : string;
 
   ngOnInit() {
   }
 
   addPrefixToSet() {
     let prefixName = this.actualPrefix.trim();
-    console.log("1" + prefixName + "1");
-    this.prefixesToSend.push(prefixName);
-    this.actualPrefix = "";
+
+    if (this.checkPrefixName(prefixName)) {
+      this.validatePrefix(prefixName);
+      this.prefixesToSend.push(prefixName);
+      this.actualPrefix = "";
+      
+      if (this.validationMessage !== "") {
+        this.validationMessage = null;
+      }
+    }
+    else {
+      this.removeLastSpaceFromPrefix();
+    }
+
+
+  }
+
+
+  checkPrefixName(name) {
+    let isValidName = true;
+
+    if (this.prefixesToSend.indexOf(name) != -1) {
+      isValidName = false;
+      this.validationMessage = "Prefiks znajduje się już w zbiorze.";
+    }
+    else if (!this.validatePrefix(name)){
+      isValidName = false;
+      this.validationMessage = "Nazwa prefiksu posiada nieprawidłowy format.";
+    }
+
+    return isValidName;
+  }
+
+  private removeLastSpaceFromPrefix() {
+    this.actualPrefix = this.actualPrefix.substring(0, this.actualPrefix.length - 1);
+  }
+
+
+  validatePrefix(name) {
+    var pattern = /^[a-z]+$/g;
+    //console.log(pattern.test(name));
+    return pattern.test(name);
+  }
+
+  removePrefixFromSet(i) {
+    this.prefixesToSend.splice(i, 1);
   }
 
 }
